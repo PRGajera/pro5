cb9d9d23a0b96ffd98409e45c344a9959afd0b71
pc <sem3b.68.tmtbca@gmail.com>
2023-02-13 12:23:54 +0530

    basic of routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
+
+// export default App;
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+render() {
+	return (
+	<Router>
+		<div className="App">
+<nav>
+			<ul className="App-header">
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/About">About Us</Link>
+			</li>
+			<li>
+				<Link to="/Contact">Contact Us</Link>
+			</li>
+			</ul>
+      </nav>
+		<Routes>
+    
+				<Route exact path='/' element={< Home />}></Route>
+				<Route exact path='/About' element={< About />}></Route>
+				<Route exact path='/Contact' element={< Contact />}></Route>
+		</Routes>
+		</div>
+	</Router>
+);
+}
 }
 
 export default App;
+
---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { Nav } from 'react-bootstrap'
+// import Nav from './Nav'
+
+
+
+function Home() {
+  return (
+    <div>
+        
+        <h1>Welcome to the world of Geeks!</h1>
+
+
+    </div>
+  )
+}
+
+export default Home
